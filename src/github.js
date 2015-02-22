1364956316a5949510e12286c4d7ec0418f6a3e9
Stephan Hoyer <ste.hoyer@gmail.com>
2015-02-22 21:40:11 +0100

    Modularize github module

--- src/github.js
+++ src/github.js
@@ -1,89 +1,74 @@
 'use strict';
 
-var reponame = 'bloghub-test';
 var Octokat = require('octokat');
-var base64 = require('base-64');
-var utf8 = require('utf8');
+var extend = require('lodash').extend;
 
-function base64encode(string) {
-  return base64.encode(utf8.encode(string));
-}
+var defaults = {
+  branchName: 'master',
+  token: '',
+  username: '',
+  reponame: ''
+};
 
-function log(thing) {
-  console.log(thing);
-  return thing;
-}
+function init(options) {
+  options = extend({}, defaults, options);
+  var head;
 
-var user = JSON.parse(localStorage.getItem('github'));
-
-var octo = new Octokat({
-  token: user.token
-});
-var repo = octo.repos(user.username, reponame);
-
-function fetchHead(branch) {
-  return repo.git.refs.heads(branch || 'master').fetch();
-}
-
-var head;
-function fetchTree(branch) {
-  return fetchHead(branch).then(function(commit) {
-    head = commit;
-    return repo.git.trees(commit.object.sha).fetch();
+  var octo = new Octokat({
+    token: options.token
   });
-}
+  var repo = octo.repos(options.username, options.reponame);
 
-function fetchReadme() {
-  return repo.contents('README.md').readBinary().then(log);
-}
+  function fetchHead() {
+    return repo.git.refs.heads(options.branchName).fetch();
+  }
 
-function commit(files, message) {
-  return Promise.all(files.map(function(file) {
-    return repo.git.blobs.create({
-      content: file.content,
-      encoding: 'utf-8'
+  function fetchTree() {
+    return fetchHead().then(function(commit) {
+      head = commit;
+      return repo.git.trees(commit.object.sha).fetch();
     });
-  })).then(function(blobs) {
-    return fetchTree().then(function(tree) {
-      return repo.git.trees.create({
-        tree: files.map(function(file, index) {
-          return {
-            path: file.path,
-            mode: '100644',
-            type: 'blob',
-            sha: blobs[index].sha
-          };
-        }),
-        base_tree: tree.sha
+  }
+
+  function commit(files, message) {
+    return Promise.all(files.map(function(file) {
+      return repo.git.blobs.create({
+        content: file.content,
+        encoding: 'utf-8'
+      });
+    })).then(function(blobs) {
+      return fetchTree().then(function(tree) {
+        return repo.git.trees.create({
+          tree: files.map(function(file, index) {
+            return {
+              path: file.path,
+              mode: '100644',
+              type: 'blob',
+              sha: blobs[index].sha
+            };
+          }),
+          baseTree: tree.sha
+        });
+      });
+    }).then(function(tree) {
+      return repo.git.commits.create({
+        message: message,
+        tree: tree.sha,
+        parents: [
+          head.object.sha
+        ]
+      });
+    }).then(function(commit) {
+      return repo.git.refs.heads(options.branchName).update({
+        sha: commit.sha
       });
     });
-  }).then(function(tree) {
-    return repo.git.commits.create({
-      message: message,
-      tree: tree.sha,
-      parents: [
-        head.object.sha
-      ]
-    });
-  }).then(function(commit) {
-    return repo.git.refs.heads('master').update({
-      sha: commit.sha
-    });
-  });
+
+  }
+
+  return {
+    commit: commit
+  };
 }
 
-commit([{
-  path: 'test/file1.md',
-  content: '# File1'
-}, {
-  path: 'test/file2.md',
-  content: '# File2'
-}], 'test commit via commit function').then(log);
-
-function commitOneFile() {
-  return repo.contents('test.md').add({
-    message: 'Updating file',
-    content: base64encode('New file contents'),
-    sha: '123456789abcdef'
-  }).then(log);
-}
+module.exports = init;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var github = require('./github');
+
 if (global.document) {
   require('es6-promise').polyfill();
 }
@@ -14,4 +16,20 @@ m.route.mode = 'hash';
 domready(function() {
   m.route(document.body , '/', routes);
 });
+
+var user = JSON.parse(localStorage.getItem('github'));
+
+var api = github({
+  username: user.username,
+  token: user.token,
+  reponame: 'bloghub-test'
+});
+
+api.commit([{
+  path: 'test/file1.md',
+  content: '# File1'
+}, {
+  path: 'test/file2.md',
+  content: '# File2'
+}], 'test commit via commit function, again, one time');
 
